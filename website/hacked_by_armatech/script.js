a457136f4bf73bb442d67865d30edc491ead90fc
Armatech_Team <armatech20@gmail.com>
2021-11-29 15:15:50 +0300

    brrrr

--- website/hacked_by_armatech/script.js
+++ website/hacked_by_armatech/script.js
@@ -1,39 +1,83 @@
 // Initialising the canvas
-var canvas = document.getElementById('matrix'),
-    ctx = canvas.getContext('2d');
+const contentEl = document.getElementById("content");
 
-// Setting the width and height of the canvas   
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+const con = {
+    write(text){
+        contentEl.innerText += text;
+    },
+    writeLine(text){
+        this.write(text + "\n");
+    },
+};
 
-// Setting up the letters
-var letters = 'ABCDEFGHIJKLMNOPQRSTUVXYZ' + "ABCDEFGHIJKLMNOPQRSTUVXYZ".toLowerCase() + "1234567890*-!'^+%&/()=?_>£#$½{[]}\\|";
-letters = letters.split('');
 
+
+
+
+
+
+
+
+
+
+
+
+
+let symbols = 'ABCDEFGHIJKLMNOPQRSTUVXYZ' + "ABCDEFGHIJKLMNOPQRSTUVXYZ".toLowerCase() + "1234567890*-!'^+%&/()=?_>£#$½{[]}\\|";
+symbols = symbols.split('');
+
+/*
 // Setting up the columns
 var fontSize = 10,
     columns = canvas.width / fontSize;
+*/
 
+/*
 // Setting up the drops
 var drops = [];
-for (var i = 0; i < columns; i++) {
+for (let i = 0; i < columns; i++) {
   drops[i] = 1;
 }
+*/
 
-// Setting up the draw function
+
+
+
+
+
+con.write("Hacked by Armatech");
+
+let bo = true;
 function draw() {
-  ctx.fillStyle = 'rgba(0, 0, 0, .1)';
-  ctx.fillRect(0, 0, canvas.width, canvas.height);
-  for (var i = 0; i < drops.length; i++) {
-    var text = letters[Math.floor(Math.random() * letters.length)];
-    ctx.fillStyle = '#0f0';
-    ctx.fillText(text, i * fontSize, drops[i] * fontSize);
-    drops[i]++;
-    if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
-      drops[i] = 0;
+
+    if(bo) {
+        con.write("_");
+    } else {
+        contentEl.innerText = contentEl.innerText.slice(0, -1); 
     }
-  }
+    bo = !bo;
+
+    if(false){
+        ctx.fillStyle = 'rgba(0, 0, 0, .1)';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        for (var i = 0; i < drops.length; i++) {
+            var text = symbols[Math.floor(Math.random() * symbols.length)];
+            ctx.fillStyle = '#0f0';
+            ctx.fillText(text, i * fontSize, drops[i] * fontSize);
+            drops[i]++;
+            if (drops[i] * fontSize > canvas.height && Math.random() > .95) {
+                drops[i] = 0;
+            }
+        }
+    }
 }
 
-// Loop the animation
-setInterval(draw, 33);
+
+
+
+
+
+
+
+setInterval(draw, 500);
+
